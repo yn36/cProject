3434d5908ae6ab736e2cf7f08f31270c7bff812c
jiajun.wu <15820330954@163.com>
2022-08-18 11:44:13 +0800

    更新

--- src/main.c
+++ src/main.c
@@ -1,16 +1,7 @@
-/*
- * @Description:
- * @Version: 1.0
- * @Autor: jiajun.wu
- * @Date: 2022-08-17 22:45:43
- * @LastEditors: jiajun.wu
- * @LastEditTime: 2022-08-17 22:54:43
- */
 #include <stdio.h>
 
 int main()
 {
-  printf("123");
 
   return 0;
 }
